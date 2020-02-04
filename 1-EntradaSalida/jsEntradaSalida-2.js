92d75f438f34152afdbacdd125ffba95bb060f58
alumno <alumno>
2020-02-03 22:22:13 -0300

    Cambios 2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var nombre;
+    nombre = prompt("Ingrese su nombre");
+    alert(nombre);
+
+    // alert(prompt("Ingrese su nombre")); ---> Otra forma de resolverlo en una sola lìnea
 }
 
