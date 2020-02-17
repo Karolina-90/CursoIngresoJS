b184f1d8bf9c454c8e7f7fb668636088e9f00c03
Karolina-90 <60630349+Karolina-90@users.noreply.github.com>
2020-02-17 12:50:52 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
+    var 
+
+    var nota = prompt("Ingrese la nota: ");
+    var sexo = prompt("Ingrese el sexo del alumno: ");
+
+    if (!(nota == 'F' || nota == 'M')) 
+
+    for(i=0;i<9;i++)
 
 }
