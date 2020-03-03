648ec16665731f4e4ad2ba23834cd35a21e7253e
alumno <alumno>
2020-03-02 22:08:10 -0300

    last act

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,14 @@
 function mostrar()
-{
+{   //Se hace con while porque es hasta que el usuario quiera
+    var letra;
+    var numero;
+
+    letra = prompt("Ingrese una letra: ");
+    numero = parseInt(prompt("Ingrese un número entre -100 y 100: "));
+    
+    while (isNaN(numero) || numero < -100 || numero > 100 ) {
+        numero = parseInt(prompt("Incorrecto! Ingrese un número entre -100 y 100: "));
+    }
+
 
 }
